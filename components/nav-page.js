42cb31a23dbd8b8eaa304927cd8e2694a87b4610
chris <dwatow@gmail.com>
2022-10-12 12:29:14 +0800

    add production category

--- components/nav-page.js
+++ components/nav-page.js
@@ -2,20 +2,21 @@ export default function navPage(app) {
 
   this.mounted = function () {
     document.querySelector('.nav-page').classList.toggle('display-none')
+    document.querySelector('.transition-next-page').classList.add('scale0-to-origin')
     setTimeout(() => {
       document.querySelectorAll('.main-nav-item').forEach(item => item.classList.toggle('scale0-to-origin'))
     })
 
     // 點擊選項，播放動畫    
     document.querySelector('.triangle').addEventListener('click', () => {
-      document.querySelector('.transition-next-page').classList.toggle('scale0-to-origin')
+      document.querySelector('.transition-next-page').classList.remove('scale0-to-origin')
       setTimeout(() => {
-        app.$router.push('/');
+        app.$router.push('product-category');
       }, 300)
       
     })
     document.querySelector('.circle').addEventListener('click', () => {
-      document.querySelector('.transition-next-page').classList.toggle('scale0-to-origin')
+      document.querySelector('.transition-next-page').classList.remove('scale0-to-origin')
       setTimeout(() => {
         app.$router.push('/');
       }, 300)
@@ -24,32 +25,32 @@ export default function navPage(app) {
 
     // 進入指定 router
     document.querySelector('.rectangle').addEventListener('click', () => {
-      document.querySelector('.transition-next-page').classList.toggle('scale0-to-origin')
+      document.querySelector('.transition-next-page').classList.remove('scale0-to-origin')
       setTimeout(() => {
-        app.$router.push('product-category');
+        app.$router.push('/');
       }, 300)
     })
   }
 
   this.render = () => `
     <div class="nav-page display-none">
-      <div class="transition-next-page scale0-to-origin"></div>
       <div class="header">
-        <img class="back-prev" src="./assets/back_prev.png" alt="">
+        <!-- 前後兩個元素隱藏 -->
+        <div></div>
         <img class="sinbon-logo" src="./assets/logo.png" alt="">
-        <img class="options" src="./assets/options.png" alt="">
+        <div></div>
       </div>
       <div class="main-nav">
         <div class="main-nav-item shape scale0-to-origin">
-          <img class="triangle" src="./assets/triangle.png" alt="">
+          <img class="triangle" src="./assets/triangle-green.png" alt="">
           <div class="text triangle-text-fix">Product</div>
         </div>
         <div class="main-nav-item shape scale0-to-origin">
-          <img class="circle" src="./assets/circle.png" alt="">
+          <img class="circle" src="./assets/circle-orange.png" alt="">
           <div class="text">Case<br />Study</div>
         </div>
         <div class="main-nav-item shape scale0-to-origin">
-          <img class="rectangle" src="./assets/rectangle.png" alt="">
+          <img class="rectangle" src="./assets/rectangle-blue.png" alt="">
           <div class="text">About<br />SINBON</div>
         </div>
       </div>
--- components/product-category.js
+++ components/product-category.js
@@ -1,7 +1,18 @@
 export default function productCategory(app) {
 
   this.mounted = function () {
-    console.log('productCategory', productCategory)
+    document.querySelector('.back-prev > img').addEventListener('click', () => {
+      document.querySelector('.transition-next-page').classList.remove('scale0-to-origin')
+      setTimeout(() => {
+        app.$router.push('/main-nav');
+      }, 300)
+    })
+    document.querySelector('.transition-next-page').classList.add('scale0-to-origin')
+
+  }
+
+  this.destroy = function () {
+    document.querySelector('.transition-next-page').classList.remove('scale0-to-origin')
   }
 
   this.render = () => `
@@ -9,9 +20,66 @@ export default function productCategory(app) {
       <div class="header">
         <div class="back-prev">
           <img src="./assets/back_prev.png" alt="">
+          <div class="title">Product Category</div>
         </div>
         <img class="sinbon-logo" src="./assets/logo.png" alt="">
         <img class="options" src="./assets/options.png" alt="">
+      </div>
+      <div class="body">
+        <div class="body-item">
+          <div class="option triangle">
+            <img class="shape" src="./assets/triangle-blue.png" alt="">
+            <span>Electric<br />Vehicle</span>
+          </div>
+          <div class="option triangle">
+            <img class="shape" src="./assets/triangle-blue.png" alt="">
+            <span>E-mobility<br />Connectivity</span>
+          </div>
+          <div class="option triangle">
+            <img class="shape" src="./assets/triangle-blue.png" alt="">
+            <span>E-bike<br />System</span>
+          </div>
+          <div class="option triangle">
+            <img class="shape" src="./assets/triangle-blue.png" alt="">
+            <span>Other<br />Mobility</span>
+          </div>
+        </div>
+        <div class="body-item">
+          <div class="option circle">
+            <img class="shape" src="./assets/circle-orange.png" alt="">
+            <span>Automation</span>
+          </div>
+          <div class="option circle">
+            <img class="shape" src="./assets/circle-orange.png" alt="">
+            <span>Robotics</span>
+          </div>
+          <div class="option circle">
+            <img class="shape" src="./assets/circle-orange.png" alt="">
+            <span>Logistics</span>
+          </div>
+          <div class="option circle">
+            <img class="shape" src="./assets/circle-orange.png" alt="">
+            <span>Micro-<br />inverter</span>
+          </div>
+          <div class="option circle">
+            <img class="shape" src="./assets/circle-orange.png" alt="">
+            <span>Multi-<br />application</span>
+          </div>
+        </div>
+        <div class="body-item">
+          <div class="option rectangle">
+            <img class="shape" src="./assets/rectangle-red.png" alt="">
+            <span>Patient<br />Monitoring</span>
+          </div>
+          <div class="option rectangle">
+            <img class="shape" src="./assets/rectangle-red.png" alt="">
+            <span>Diagnostics<br />Imaging</span>
+          </div>
+          <div class="option rectangle">
+            <img class="shape" src="./assets/rectangle-red.png" alt="">
+            <span>Medical<br />Robots</span>
+          </div>
+        </div>
       </div>
     </div>`
 }
