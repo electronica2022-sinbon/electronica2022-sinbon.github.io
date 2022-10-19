2bd14c99259137ad990a59f778ac855dd6af6377
chris <dwatow@gmail.com>
2022-10-19 11:24:11 +0800

    add autoplay video, replay video in casy example

--- src/components/case-example.js
+++ src/components/case-example.js
@@ -8,9 +8,18 @@ export default function caseExample(app) {
   //   }, 10)
   // }
 
+  const showContentAfterVideoEnd = () => {
+    setTimeout(() => {
+      document.querySelectorAll('.hidden').forEach(item => {
+        item.classList.add('show')
+        item.classList.remove('hidden')
+      })
+    }, 8000)
+  }
+
   this.mounted = function () {
     document.querySelector('.transition-next-page').classList.add('scale-0')
-    // setTimeout(removeInitialState, 10)
+    showContentAfterVideoEnd()
 
     // const fields = current_data(index).fields
     // console.log('render markdown', app.$md.render(fields['Description']));
@@ -20,10 +29,24 @@ export default function caseExample(app) {
   }
   
   this.bindingEvent = () => {
+    document.querySelector('video').addEventListener('loadeddata', (e) => {
+      // https://jiepeng.me/2019/03/17/autoplay-policy-note
+      e.currentTarget.play()
+    })
+    document.querySelector('.replay-button').addEventListener('click', () => {
+      document.querySelectorAll('.show').forEach(item => {
+        item.classList.add('hidden')
+        item.classList.remove('show')
+      })
+      document.querySelector('video').play()
+      showContentAfterVideoEnd()
+    })
     // after render
     document.querySelector('.icon-close').addEventListener('click', () => {
       app.$router.push('case-study');
     })
+
+
   }
 
   this.render = () => `
@@ -36,7 +59,19 @@ export default function caseExample(app) {
         <img class="icon-close" src="./src/assets/close-white.png" alt="">
       </div>
       <div class="container">
-        <video preload="auto" src="./src/video/case1.mp4" autoplay></video>
+        <video muted preload="auto" src="./src/video/case1.mp4" autoplay></video>
+        <div class="background hidden"></div>
+        <div class="content hidden">
+          <div class="content-options">
+            <div class="content-option active">Challenge</div>
+            <div class="content-option">Solution</div>
+          </div>
+          <div class="content-text">
+            The umbilical cable assembly requires the 78-pin plug connector on both ends, and the cable and connectors must serve as
+            a sturdy bridge for high-definition cardiac mapping capital equipment.
+            <img class="replay-button" src="./src/assets/replay.png" alt="">
+          </div>
+        </div>
       </div>
     </div>`
 }
