d2479203b8f5ec4e24203714001548362fc6212c
chris <dwatow@gmail.com>
2022-11-08 11:44:22 +0800

    modify case example

--- src/components/case-example.js
+++ src/components/case-example.js
@@ -132,7 +132,7 @@ export default function caseExample(app) {
 
   this.renderContentText = (index) => {
     if (index == null) {
-      return `${thisCase().challenge}<img class="replay-button" src="./src/assets/button-replay.png" alt="">`
+      return `${app.$md.render(thisCase().challenge)}<img class="replay-button" src="./src/assets/button-replay.png" alt="">`
     } else {
       const solution = thisCase().solutions[index];
       if ('features' in solution) {
